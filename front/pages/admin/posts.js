21f082d87ab740fd254f35b123dc1a298f52962f
kapable <skjoldenstore@gmail.com>
2022-02-28 22:14:03 +0900

    add link copy in admin posts list

--- front/pages/admin/posts.js
+++ front/pages/admin/posts.js
@@ -1,13 +1,18 @@
-import React, { Fragment, useCallback, useEffect } from 'react';
+import React, { Fragment, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Table, Space, Image } from 'antd';
+import { LinkOutlined } from '@ant-design/icons';
 import { REMOVE_POST_REQUEST } from '../../reducers/post';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const { Column, ColumnGroup } = Table;
 
 const PostList = () => {
     const dispatch = useDispatch();
     const { mainPosts } = useSelector((state) => state.post);
+    const onShareButtonClick = useCallback(() => {
+        alert('링크가 복사되었습니다!');
+    }, []);
     
     const onRemovePost = useCallback((id) => {
         if (confirm("정말로 삭제하시겠습니까?\n삭제된 게시물은 복구가 불가능합니다.") === true) {
@@ -22,8 +27,17 @@ const PostList = () => {
             <Table pagination={{ pageSize: 10 }} dataSource={mainPosts} key="table" rowKey={post => post.id}>
                 <Column title="No." dataIndex="id" key="post-id" />
                 <Column title="제목" dataIndex="title" key="title" />
+                <Column
+                    title="링크" key="link-copy"
+                    render={(_, post) => (
+                        <CopyToClipboard
+                            text={`https://niair.xyz/post/${post.id}`}
+                            onCopy={onShareButtonClick}
+                        ><div><LinkOutlined /></div></CopyToClipboard>
+                    )}
+                />
                 <Column title="미리보기" key="preview" render={(_, post) => (
-                    <Image width={100} src={post.Images[0].src} alt={post.title} />
+                    <Image width={70} src={post.Images[0].src} alt={post.title} />
                 )}/>
                 <ColumnGroup title="수정/삭제">
                     <Column
