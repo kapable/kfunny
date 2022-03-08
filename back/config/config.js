15e556f4cfa0a62510e0b49b29df692279a0a751
kapable <skjoldenstore@gmail.com>
2022-03-08 18:29:22 +0900

    fix rds connection

--- back/config/config.js
+++ back/config/config.js
@@ -4,17 +4,11 @@ dotenv.config();
 
 module.exports = {
   "development": {
-    // "username": "root",
-    // "password": process.env.DB_PASSWORD,
-    // "database": "kfunny_development",
-    // "host": "127.0.0.1",
-    // "dialect": "mysql"
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DATABASE,
-    "host": process.env.RDS_HOST,
-    "dialect": "mysql",
-    "port":process.env.RDS_PORT
+    "username": "root",
+    "password": process.env.DB_PASSWORD,
+    "database": "kfunny_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
