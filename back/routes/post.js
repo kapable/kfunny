77e2a1c7dc000808e84e4dcd912d755eb2dc130b
kapable <skjoldenstore@gmail.com>
2022-03-10 17:57:36 +0900

    fix s3 content type

--- back/routes/post.js
+++ back/routes/post.js
@@ -35,7 +35,8 @@ const upload = multer({
         bucket: 'kfunny-image-s3',
         key(req, file, cb) {
             cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)
-        }
+        },
+        contentType: multerS3.AUTO_CONTENT_TYPE
     }),
     limits: { fileSize: 20 * 1024 * 1024 },
 });
