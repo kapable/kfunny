9e61edfb548147441744403c76e94e70a268b69b
kapable <skjoldenstore@gmail.com>
2022-03-10 22:16:03 +0900

    fix: meta tag for id page

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -13,16 +13,16 @@ const App = ({ Component }) => {
         <Fragment>
             <Head>
                 <title>케이퍼니</title>
-                <link rel='main-url' href='https://niair.xyz' />
+                {/* <link rel='main-url' href='https://niair.xyz' />
                 <link rel='shortcut icon' href='/favicon.png'/>
                 <meta charSet='utf-8'/>
                 <meta name="language" content="Korean" />
                 <meta name="author" content="쿠키로켓" />
                 <meta name="description" content="핫이슈가 모인 최신 미디어, 케이퍼니" />
-                <meta name="keywords" content="핫이슈, 뉴스, 최신 트렌드, 정보, 웃긴, 돈되는, 케이퍼니, 케이퍼티" />
+                <meta name="keywords" content="핫이슈, 뉴스, 최신 트렌드, 정보, 웃긴, 돈되는, 케이퍼니, 케이퍼티" /> */}
 
                 {/* <!-- Open Graph / Facebook --> */}
-                <meta property="og:type" content="website"/>
+                {/* <meta property="og:type" content="website"/>
                 <meta property="og:url" content='https://niair.xyz' />
                 <meta property="og:title" content='케이퍼니'/>
                 <meta property="og:description" content="핫이슈가 모인 최신 미디어, 케이퍼니"/>
@@ -30,10 +30,10 @@ const App = ({ Component }) => {
                 <meta property="og:image:width" content="800"/>
                 <meta property="og:image:height" content="400"/>
                 <meta property="og:image:alt" content="핫이슈가 모인 최신 미디어, 케이퍼니" />
-                <meta property='og:site_name' content='케이퍼니' />
+                <meta property='og:site_name' content='케이퍼니' /> */}
 
                 {/* <!-- Twitter --> */}
-                <meta property="twitter:card" content="summary_large_image"/>
+                {/* <meta property="twitter:card" content="summary_large_image"/>
                 <meta property="twitter:url" content="https://niair.xyz/login"/>
                 <meta property="twitter:title" content='케이퍼니'/>
                 <meta property="twitter:description" content="핫이슈가 모인 최신 미디어, 케이퍼니"/>
@@ -41,7 +41,7 @@ const App = ({ Component }) => {
                 <meta property="twitter:image:width" content="800"/>
                 <meta property="twitter:image:height" content="400"/>
                 <meta property="twitter:image:alt" content="핫이슈가 모인 최신 미디어, 케이퍼니" />
-                <meta property='og:site_name' content='케이퍼니' />
+                <meta property='og:site_name' content='케이퍼니' /> */}
             </Head>
             <AppLayout>
                 <Component />
