27bb55eab9046142c3c27b883cbf171bb4eef16a
kapable <skjoldenstore@gmail.com>
2022-03-10 22:40:43 +0900

    fix: meta tag for index page

--- front/pages/index.js
+++ front/pages/index.js
@@ -8,6 +8,7 @@ import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 import { END } from 'redux-saga';
 import wrapper from '../store/configureStore';
 import axios from 'axios';
+import Head from 'next/head';
 
 const { TabPane } = Tabs;
 
@@ -50,6 +51,38 @@ const Home = () => {
     
     return (
         <Fragment>
+            <Head>
+                <title>케이퍼니</title>
+                <link rel='main-url' href='https://niair.xyz' />
+                <link rel='shortcut icon' href='/favicon.png'/>
+                <meta charSet='utf-8'/>
+                <meta name="language" content="Korean" />
+                <meta name="author" content="쿠키로켓" />
+                <meta name="description" content="핫이슈가 모인 최신 미디어, 케이퍼니" />
+                <meta name="keywords" content="핫이슈, 뉴스, 최신 트렌드, 정보, 웃긴, 돈되는, 케이퍼니, 케이퍼티" />
+
+                {/* <!-- Open Graph / Facebook --> */}
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content='https://niair.xyz' />
+                <meta property="og:title" content='케이퍼니'/>
+                <meta property="og:description" content="핫이슈가 모인 최신 미디어, 케이퍼니"/>
+                <meta property="og:image" content="https://ktestone.com/static/media/main-header.21ffb6bb.png"/>
+                <meta property="og:image:width" content="800"/>
+                <meta property="og:image:height" content="400"/>
+                <meta property="og:image:alt" content="핫이슈가 모인 최신 미디어, 케이퍼니" />
+                <meta property='og:site_name' content='케이퍼니' />
+
+                {/* <!-- Twitter --> */}
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://niair.xyz/login"/>
+                <meta property="twitter:title" content='케이퍼니'/>
+                <meta property="twitter:description" content="핫이슈가 모인 최신 미디어, 케이퍼니"/>
+                <meta property="twitter:image" content="https://ktestone.com/static/media/main-header.21ffb6bb.png"/>
+                <meta property="twitter:image:width" content="800"/>
+                <meta property="twitter:image:height" content="400"/>
+                <meta property="twitter:image:alt" content="핫이슈가 모인 최신 미디어, 케이퍼니" />
+                <meta property='og:site_name' content='케이퍼니' />
+            </Head>
             <Tabs tabPosition='top' size='default' type='line' onChange={onChangeCategory}>
                 {postCategories.map((category, _) => {
                     if(category.enabled) {
