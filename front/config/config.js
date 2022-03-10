738bffcdb4f0eb822734b3ebd7e540cba97cb153
kapable <skjoldenstore@gmail.com>
2022-03-10 12:09:31 +0900

    apply test backUrl

--- front/config/config.js
+++ front/config/config.js
@@ -1,7 +1,7 @@
 export let backUrl;
 
 if(process.env.NODE_ENV === 'development') {
-    backUrl = 'http://localhost:3065';
+    backUrl = 'http://13.209.4.3:3065';
 } else {
     backUrl = 'https://api.niair.xyz';
 }
