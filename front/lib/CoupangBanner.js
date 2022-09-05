15148d8451b40bf5f9888b099696433e467ccf91
kapable <skjoldenstore@gmail.com>
2022-09-05 15:52:11 +0900

    change coupangBanner script to iframe

--- front/lib/CoupangBanner.js
+++ front/lib/CoupangBanner.js
@@ -3,15 +3,15 @@ import React from 'react';
 const CoupangBanner = () => {
     return (
         <>
-            <script src="https://ads-partners.coupang.com/g.js"></script>
+            {/* <script src="https://ads-partners.coupang.com/g.js"></script>
             <script
                 type="text/javascript"
                 dangerouslySetInnerHTML={{
                     __html:`
                     new PartnersCoupang.G({"id":593521,"template":"carousel","trackingCode":"AF4396324","width":"680","height":"140"});
                     `
-            }}/>
-            {/* <iframe src="https://ads-partners.coupang.com/widgets.html?id=593521&template=carousel&trackingCode=AF4396324&subId=&width=680&height=140" width="680" height="140" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe> */}
+            }}/> */}
+            <iframe src="https://ads-partners.coupang.com/widgets.html?id=593521&template=carousel&trackingCode=AF4396324&subId=&width=360&height=140" width="360" height="140" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
         </>
     );
 };
