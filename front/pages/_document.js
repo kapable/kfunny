7a9238594f1f656d2ffd2c9615973dba53d0b8c0
kapable <skjoldenstore@gmail.com>
2022-09-29 12:00:35 +0900

    add adsense script

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -49,6 +49,7 @@ export default class MyDocument extends Document {
                 }}/>
             <script async src="//cmp.optad360.io/items/ffa06b17-51f0-42df-b756-cb8ce7945986.min.js"></script>
             <script async src="//get.optad360.io/sf/220513d0-b3fa-450b-8eec-34169517d936/plugin.min.js"></script>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2382342018701919" crossorigin="anonymous"></script>
                 <body>
                     <Main />
                     {/* <script src='https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019' /> */}
