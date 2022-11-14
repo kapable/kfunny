bf2bdf2f0d9f212bbdaf939592baaf474fb160dc
kapable <skjoldenstore@gmail.com>
2022-11-14 21:59:38 +0900

    fix dynamic metatag for article page

--- front/pages/article/[id].js
+++ front/pages/article/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { END } from 'redux-saga';
 import axios from 'axios';
@@ -17,14 +17,20 @@ const Article = () => {
     const { singleArticle } = useSelector((state) => state.article);
 
     const imgRegex = new RegExp('<img src="(.*?)"', 'igm');
-    const thumbnailSrc = useState(imgRegex.exec(singleArticle.contents)[1].toString() || ['https://images.niair.xyz/basic/kfunny_logo.png'] );
+    const [thumbnailSrc, setThumbnailSrc] = useState('');
 
     const textRegex = new RegExp('<\s*p[^>]*>([^<]*)<\s*\/\s*p\s*>', 'igm');
-    const textDesc = useState(textRegex.exec(singleArticle.contents)[1].toString() || ["쿠키로켓, 핫이슈, 뉴스, 최신 트렌드, 정보, 웃긴, 돈되는, 케이퍼니, 케이퍼티"])
+    const [textDesc, setTextDesc] = useState("");
+
+    useEffect(() => {
+        setThumbnailSrc(imgRegex.exec(singleArticle?.contents));
+        setTextDesc(textRegex.exec(singleArticle?.contents));
+    }, [singleArticle]);
 
     return (
         <Fragment>
             <Head>
+                {console.log()}
                 <title>{singleArticle?.title} | 케이퍼니</title>
                 <meta charSet='utf-8'/>
                 <meta name='desciprtion' content={singleArticle?.title}/>
@@ -35,8 +41,8 @@ const Article = () => {
 
                 {/* <!-- Open Graph / Facebook --> */}
                 <meta property='og:title' content={`${singleArticle.title} | 케이퍼니`}/>
-                <meta property='og:description' content={textDesc[0]}/>
-                <meta property='og:image' content={thumbnailSrc[0]}/>
+                <meta property='og:description' content={textDesc ? textDesc[1] : "쿠키로켓, 핫이슈, 뉴스, 최신 트렌드, 정보, 웃긴, 돈되는, 케이퍼니, 케이퍼티"}/>
+                <meta property='og:image' content={thumbnailSrc ? thumbnailSrc[1] : 'https://images.niair.xyz/basic/kfunny_logo.png'}/>
                 <meta property='og:url' content={`https://niair.xyz/article/${id}`}/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:image:width" content="800"/>
@@ -47,8 +53,8 @@ const Article = () => {
                 {/* <!-- Twitter --> */}
                 <meta property="twitter:card" content="summary_large_image"/>
                 <meta property='twitter:title' content={`${singleArticle.title} | 케이퍼니`}/>
-                <meta property='twitter:description' content={textDesc[0]}/>
-                <meta property='twitter:image' content={thumbnailSrc[0]}/>
+                <meta property='twitter:description' content={textDesc ? textDesc[1] : "쿠키로켓, 핫이슈, 뉴스, 최신 트렌드, 정보, 웃긴, 돈되는, 케이퍼니, 케이퍼티"}/>
+                <meta property='twitter:image' content={thumbnailSrc ? thumbnailSrc[1] : 'https://images.niair.xyz/basic/kfunny_logo.png'}/>
                 <meta property='twitter:url' content={`https://niair.xyz/article/${id}`}/>
                 <meta property="twitter:type" content="website"/>
                 <meta property="twitter:image:width" content="800"/>
