eb67d09a69b6b96f0146e89d4d4afe52bf7dd0f7
kapable <skjoldenstore@gmail.com>
2023-01-09 15:16:30 +0900

    minor fix

--- front/components/ArticleCardContentsForm.js
+++ front/components/ArticleCardContentsForm.js
@@ -18,7 +18,7 @@ const ArticleCardContentsForm = () => {
     const { managingUrls } = useSelector((state) => state.url);
     
     const adProb = Math.random() < 1.1;
-    let originAdProb = Math.random > 0.1;
+    let originAdProb = Math.random > -1;
     const otherAdProb = Math.random() < 0.8;
     const coupangLinks = managingUrls.filter((l) => l.name.includes("쿠팡"));
     const isOtherCoupangLink = router.query?.ref_id && coupangLinks.filter((l) => l.name.replace('쿠팡파트너스','') === router.query?.ref_id).length > 0 && otherAdProb;
